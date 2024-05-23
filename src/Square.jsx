c0d2e309b6449ce958b383cf6a026e76b64ba5c9
Sam Harradine <124873582+Equallizerr1@users.noreply.github.com>
2024-05-23 15:36:01 +0000

    Initial commit

---
+++ src/Square.jsx
@@ -0,0 +1,16 @@
+//import { useState } from "react"
+
+export default function Square({ value, onSquareClick }) {
+    //const [value, setValue] = useState(null);
+
+    // function handleClick() {
+    //     setValue('X')
+    // }
+
+    return (
+        <button className="square"
+            // onClick={handleClick}
+            onClick={onSquareClick}
+        >{value}</button>
+    )
+}
